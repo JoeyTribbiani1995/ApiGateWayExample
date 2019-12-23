1223cf5544b175503c988c024c0c925cca4a86b0
JoeyTribbiani <vophanhongdung@gmail.com>
2019-12-23 20:26:16 +0700

    done commit

---
+++ CatalogApi/Controllers/CurrenciesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrenciesController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "value";
+        }
+    }
+}
